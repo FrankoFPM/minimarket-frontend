2ab0a8198b52f29f049b053eccda642e20863a33
FrankoFPM <frank.13.gp@gmail.com>
2025-05-23 18:02:01 -0500

    Agregar layout de administración con navegación y componentes de tabla; implementar panel de productos

---
+++ app/routes/Admin/Components/LayoutComponets.tsx
@@ -0,0 +1,96 @@
+import { FaBox, FaChartPie, FaClipboardList, FaCog, FaHome, FaShoppingCart, FaUser, FaUsers } from 'react-icons/fa'
+import { Link, useLocation } from 'react-router'
+import navigationData from '../navigation.json'
+import { GiDinosaurRex } from 'react-icons/gi'
+
+export function Header(){
+
+  const path = useLocation().pathname
+
+  let title = 'Bienvenido de nuevo, USER'
+  let subtitle: React.ReactNode = 'Revise el balance que tenemos preparado para ti'
+
+  if(path !== '/dashboard'){
+
+    title = navigationData.find(item => item.link === path)?.text || 'Dashboard'
+    subtitle = (
+      <div className='flex gap-2 font-semibold text-sm'>
+        <Link to={'/dashboard'} className='text-gray-400'>Dashboard</Link>
+        <span>/</span>
+        <span>{title}</span>
+      </div>
+    )
+  }
+
+  return (
+    <header className="header flex justify-between items-start pt-7">
+      <div>
+        <h1 className="text-3xl font-bold capitalize">{title}</h1>
+        <span className="">{subtitle}</span>
+      </div>
+      <div className="pr-8 flex items-center gap-4">
+        <input type="text" className="bg-secondary text-foreground h-10 rounded-2xl px-4 border-1 border-foreground/10" placeholder="Buscar" />
+        <FaCog className="text-2xl text-primary-1 cursor-pointer hover:rotate-180 transition-transform" />
+        <div className='cursor-pointer hover:bg-secondary p-2 rounded-xl text-primary-1 flex items-center gap-2'>
+          <FaUser className="text-2xl text-primary-1" />
+          <p className="m-0 leading-none font-semibold">Cerrar Sesión</p>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+const icons = {
+  home: <FaHome />,
+  GiDinosaurRex  : <GiDinosaurRex  />,
+  FaClipboardList : <FaClipboardList  />,
+  FaShoppingCart : <FaShoppingCart  />,
+  FaBox : <FaBox  />,
+  FaChartPie : <FaChartPie  />,
+  FaCog : <FaCog  />,
+  FaUsers : <FaUsers  />,
+}
+
+type IconKeys = keyof typeof icons;
+
+interface ButtonProps {
+    link: string;
+    icon: React.ReactNode;
+    text: string;
+    active: boolean;
+}
+
+export function Button({link, icon, text, active}: ButtonProps){
+  return (
+    <Link to={link} className={`w-full h-16 cursor-pointer text-foreground p-2 rounded-xl 
+             flex items-center gap-3 font-semibold drop-shadow-xs transition-colors
+            ${active ? 'bg-secondary duration-300' : 'hover:bg-secondary duration-200'}`}>
+      <div className={`transition-colors duration-300 h-9 w-9 flex items-center justify-center rounded-xl ${active ? 'bg-primary-1 text-secondary' : 'bg-secondary text-primary-1'}`}>
+        {icon}
+      </div>
+      <p className={`transition-colors duration-300 ${active ? 'text-foreground' : 'text-gray-400'}`}>
+        {text}
+      </p>
+    </Link>
+  )
+}
+
+export function Navbar(){
+  const path = useLocation().pathname
+  return (
+    <nav className="w-fit h-full px-6 nav">
+      <div className='flex items-center gap-3 my-6 px-4'>
+        <img src="/images/Logo.webp" alt="Logo" width={200} height={70}/>
+      </div>
+      <div className='h-0.5 bg-gradient-to-r from-primary-1/25 via-primary-1 to-primary-1/25 w-full mb-6 rounded-4xl'>
+      </div>
+      <div className='flex flex-col gap-2'>
+        {navigationData.map((item, index) => (
+          <Button key={index} link={item.link} icon={icons[item.icon as IconKeys]} text={item.text} active={(path == item.link ? true : false)} />
+        ))}
+        <div className='h-0.5 bg-gradient-to-r from-primary-1/25 via-primary-1 to-primary-1/25 w-full rounded-4xl'></div>
+
+      </div>
+    </nav>
+  )
+}
