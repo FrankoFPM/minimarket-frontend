cb85fca854002485673cdd923d2c78e732a0064f
Genesis <genesiscob.03@gmail.com>
2025-06-04 18:52:38 -0500

    agregue rutas y edite la gestion de adminstracion  en todas las seccion

--- app/routes/Admin/suppliers.tsx
+++ app/routes/Admin/suppliers.tsx
@@ -1,27 +1,33 @@
 import { ButtonsActions, ChipStatus, Table } from './Components/Table'
 
-export default function SalesIndex() {
+export default function SuppliersIndex() {
 
   const headers = [
-    { text: 'ID', className: 'text-center' },
-    { text:'Nombre', className: 'text-left' },
-    { text:'Proovedor', className: 'text-left' },
-    { text: 'Fecha de modificacion', className: 'text-left' },
+    { text: '#', className: 'text-center' },
+    { text:'Proovedores', className: 'text-left' },
+    { text:'Producto', className: 'text-left' },
+    { text: 'Fecha de compra', className: 'text-left' },
+    { text: 'Cantidad', className: 'text-left' },
+    { text: 'Precio unitario', className: 'text-left' },
+    { text: 'Total', className: 'text-left' },
     { text: 'Estado', className: 'text-center' },
     { text: 'Acciones', className: 'text-center' },
   ]
 
   return (
     <div className="flex flex-col bg-background mx-auto my-10 container gap-4">
-      <h1 className="text-3xl font-bold text-center">Gestión de Ventas</h1>
-      <p className="text-center">Aquí puedes gestionar todas las ventas de tu tienda.</p>
+      <h1 className="text-3xl font-bold text-center">Gestión de Proovedores</h1>
+      <p className="text-center">Aquí puedes gestionar todos los proovedores de tu tienda.</p>
       <Table headers={headers}>
 
         <tr className="[&>td]:h-12 [&>td]:px-4 [&>td]:py-1.5">
           <td className="text-center" width={160}>1</td>
+          <td>Proveedor xyz</td>
           <td>Manzana</td>
-          <td>Proovedor SAC</td>
-          <td>20/03/25</td>
+          <td>10/03/25</td>
+          <td>100</td>
+          <td>S/.1.50</td>
+          <td>S/.135.00</td>
           <td className="">
             <ChipStatus status={1} />
           </td>
