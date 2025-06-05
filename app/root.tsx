80fbdd13e0c3993132a12cf49d2a47c3dfe5f01a
FrankoFPM <frank.13.gp@gmail.com>
2025-06-04 21:28:46 -0500

    Add @heroui/react dependency to package.json

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from './+types/root'
 import './app.css'
+import { HeroUIProvider } from '@heroui/system'
 
 export const links: Route.LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -68,7 +69,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />
+  return (
+    <HeroUIProvider>
+      <Outlet />
+    </HeroUIProvider>
+  )
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
